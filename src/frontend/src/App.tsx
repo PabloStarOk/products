91800b325bb90193e49e8736085c6a61c2f7b8f4
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2026-05-26 09:46:33 -0500

    feat(frontend): add bar to search for products

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -3,14 +3,30 @@ import "@/App.css";
 import ProductsList from "@/components/ProductsList";
 import type { Product } from "@/models/product";
 import { productService } from "@/services/product-service";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [search, setSearch] = useState("");
+
   useEffect(() => {
     productService.getAll().then(setProducts);
   }, []);
 
-  return <ProductsList products={products} />;
+  const filteredProducts = products.filter((p) => {
+    const query = search.trim().toLowerCase();
+    return (
+      p.name?.toLowerCase().includes(query) ||
+      p.sku?.toLowerCase().includes(query)
+    );
+  });
+
+  return (
+    <main>
+      <SearchBar onSearch={setSearch} />
+      <ProductsList products={filteredProducts} />
+    </main>
+  );
 }
 
 export default App;
